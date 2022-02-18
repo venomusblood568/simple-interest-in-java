cb20b98ea7af374c3c6e9d9cebc0f7e76c984eb8
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2022-02-18 16:00:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.coding;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        this is simple program that is calculating simple interest
+         */
+        float p,r,t,si;
+        p = 13000;
+        r = 12;
+        t = 2;
+        si = (p*r*t)/100;
+        System.out.println("simple interest is: "+si);
+
+    }
+}
